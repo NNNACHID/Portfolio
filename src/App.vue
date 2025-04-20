6c5d74c552c3e7429c4b24be8f133b5bb805b224
D1sph0l1dus <76053592+NNNACHID@users.noreply.github.com>
2025-04-21 00:22:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+  <div class="p-10 px-20">
+    <Navbar/>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex justify-between items-center">
+        <h1 class="text-3xl font-bold">Nicolas Nachid</h1>
+        <ul class="flex space-x-4">
+            <li>Home</li>
+            <li>About</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
